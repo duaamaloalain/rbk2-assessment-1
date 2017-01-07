bd7aa6ff4f4ac3b872ae81143bdb440a8ac564b5
Dua'a Maloalain <maloalain.duaa94@hotmail.com>
2017-01-07 10:13:06 +0200

    addRemove

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,16 @@
 var Stack = function() {
-	//your code is here
+	var _storage = Object.create(arrayMethod);
+	this.array=[];
+	this.value=null; 
+	return _storage ; 
 };
+
+var arrayMethod = {}
+
+arrayMethod.add = function(value) {
+	this.array.push(value); 
+}
+
+arrayMethod.remove=function(value) {
+	this.array.splice(this.array.indexOf(value),1)
+}
